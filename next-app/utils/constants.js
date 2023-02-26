3422ee8d7f67c50a33d522edc0338cc477ae35bc
ivarslevans <ivars.levans@gmail.com>
2023-02-26 20:59:56 +0200

    Local

--- next-app/utils/constants.js
+++ next-app/utils/constants.js
@@ -1,8 +1,8 @@
 import ContractABI from './Instagram.json'
 import Web3 from 'web3'
 
-export const address = '0xDf0c223c5155054d46C15Ea2cF68c5c6FC3692E3' // goerli
-// export const address = '0xBACAb3990b7bBA0D287C7A72A03597E48d4b104f' // local
+// export const address = '0xDf0c223c5155054d46C15Ea2cF68c5c6FC3692E3' // goerli
+export const address = '0xBACAb3990b7bBA0D287C7A72A03597E48d4b104f' // local
 
 export const createContract = () => {
   const { ethereum } = window
